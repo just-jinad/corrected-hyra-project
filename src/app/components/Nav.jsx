9e641b3bc13cc139c0ea795f408c0a0f1a779ef4
Jinad Tope Isaac <jinadtope66@gmailcom>
2024-06-13 16:14:07 +0100

    update

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,35 +1,79 @@
 "use client";
+import { useState } from 'react';
 import { Button, Navbar } from "flowbite-react";
-import React from 'react';
 import Link from 'next/link';
-const Navbarcom = () => {
-  return (
-    <div>
-    <Navbar fluid rounded className="fixed bg-white w-full">
-      <Navbar.Brand href="https://flowbiteact.com">
-        <span className="self-center text-2xl font-semibold font-inter text-blue-700">Hyra</span>
-      </Navbar.Brand>
-      <div className="flex md:order-2">
-        <Navbar.Toggle />
-      </div>
-      <div className="flex justify-between -mt-6 w-full">
-      <Navbar.Collapse className="mx-auto mt-2 md:mt-0 ">
-        <div className="md:flex grid  bg-slate-100 md:bg-white justify-between w-fll py-4 px-2 md:w-72 md:ms-24">
-        <Link href="/" className="text-[15px] mt-4 md:mt-0">Home</Link>
-        <Link href="/ourteaching" className="text-[15px] mt-4 md:mt-0">Our Teachings</Link>
-        <Link href="/about" className="text-[15px] mt-4 md:mt-0" >About Us</Link>
-        </div>
-      </Navbar.Collapse>
-      <div className="float-end">
-      <div className="md:block hidden">
-         <Link href="/signup" type="button" className="text-blue-700 border border-blue-700 focus:ring-4 focus:outline-none font-semibold rounded-lg text-sm px-4 py-2 text-center ">Sign Up</Link>
-            <button type="button" className="text-white bg-blue-900 py-2 ms-1 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-lg text-sm px-6 text-center "><Link href="/login">Log in</Link></button>
-         </div>
-      </div>
-      </div>
 
-    </Navbar>
-    </div>
+const Navbarcom = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <>
+      <nav className="bg-white border-gray-200">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-blue-500">Hyra</span>
+          </a>
+          <button
+            onClick={toggleMenu}
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:focus:ring-gray-60"
+            aria-controls="navbar-default"
+            aria-expanded={isMenuOpen}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+            </svg>
+          </button>
+          <div className={`${isMenuOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
+            <ul className="font-medium flex flex-col items-center p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
+              <li>
+                <a  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">
+                  <Link href="/">
+                  Home
+                  </Link>
+                  </a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-dark md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  
+                <Link href="/ourteaching">
+                  Our Teaching
+                  </Link>
+                  </a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-dark md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  <Link href="/about">
+                  About Us
+                  </Link>
+                </a>
+              </li>
+              <div className="flex flex-col space-y-2 md:hidden">
+                <Link href="/signup">
+                  Sign Up
+                </Link>
+                <Link href="/login">
+                  Login
+                </Link>
+              </div>
+            </ul>
+          </div>
+          <div className="hidden md:flex space-x-2">
+          <Link href="/signup">
+              <Button className="text-white bg-blue-900 hover:bg-blue-800 font-semibold rounded-lg text-sm px-6 py-2">Sign up</Button>
+            </Link>
+            <Link href="/login">
+              <Button className="text-blue-700 border border-blue-700 font-semibold rounded-lg text-sm px-4 py-2">Login</Button>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 };
 
